763fece8bab2e07d8f0258c20cb72335b4ea1440
𒀳 Scribe of the Ziggurat𒅆𒂍𒉪 <steve@raffmail.net>
2025-12-27 21:47:24 +1000

    add datastar.http.zig compatible benchmarks

---
+++ bench/README.md
@@ -0,0 +1,11 @@
+# Zig 0.16-dev Benchmark
+
+To build - run `make`
+
+Then run the benchmark
+
+This benchmark is compatible with
+
+https://github.com/zigster64/datastar.http.zig/bench
+
+... so it provides some numbers for comparison with the (more mature) datastar.http.zig SDK
---
+++ bench/bench.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+const datastar = @import("datastar");
+const HTTPRequest = datastar.HTTPRequest;
+const Io = std.Io;
+
+pub fn main() !void {
+    const gpa = std.heap.smp_allocator;
+
+    // Evented isnt really working yet, so stick with Threaded IO for now
+    // Once Evented is functional, its just a 1 line change here to swap
+    // from heavy threads to coroutines
+    var threaded: Io.Threaded = .init(gpa);
+    defer threaded.deinit();
+    threaded.setAsyncLimit(std.Io.Limit.limited64(10));
+    const io = threaded.io();
+
+    var server = try datastar.Server.init(io, gpa, "0.0.0.0", 8090);
+    defer server.deinit();
+
+    const r = server.router;
+    try r.get("/", handler);
+    try r.get("/log", handlerLogged);
+    try r.get("/sse", sseHandler);
+
+    std.debug.print("Zig Datastar 0.16-dev SSE Server running at http://localhost:8090\n", .{});
+    try server.run();
+}
+
+pub fn handler(http: HTTPRequest) !void {
+    return http.html(@embedFile("index.html"));
+}
+
+pub fn handlerLogged(http: HTTPRequest) !void {
+    var t1 = try std.time.Timer.start();
+    defer {
+        std.debug.print("Zig index handler took {} microseconds\n", .{t1.read() / std.time.ns_per_ms});
+    }
+    return http.html(@embedFile("index.html"));
+}
+
+pub fn sseHandler(http: HTTPRequest) !void {
+    var t1 = try std.time.Timer.start();
+    defer {
+        std.debug.print("Zig SSE handler took {} microseconds\n", .{t1.read() / std.time.ns_per_ms});
+    }
+    var buf: [1024]u8 = undefined;
+    var sse = try datastar.NewSSE(http, &buf);
+    defer sse.close();
+
+    try sse.patchElements(@embedFile("index.html"), .{});
+}
